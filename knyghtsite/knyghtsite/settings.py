76d3bc6e44ebf18e1662cd1bd2e92fba17bc9557
knyghtmare <tahsinjahinkhalid@gmail.com>
2020-01-03 21:35:48 +0600

    added the second day's codes

--- knyghtsite/knyghtsite/settings.py
+++ knyghtsite/knyghtsite/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'crispy_forms',
+
     'main.apps.MainConfig',
+    'register.apps.RegisterConfig',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4' 
--- knyghtsite/knyghtsite/urls.py
+++ knyghtsite/knyghtsite/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from register import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', views.register, name='register'),
     path('home/', include('main.urls')),
 ]
